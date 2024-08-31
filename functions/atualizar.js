9d265a33e98ebf96d08de9738a5b583fbf64812d
isaias goncalves <isaiasgr575@gmail.com>
2024-08-31 19:37:46 -0300

    Finalizando

--- functions/atualizar.js
+++ functions/atualizar.js
@@ -3,7 +3,7 @@ const atualizar = document.getElementById("atualizar");
 async function listar() {
   const api = await fetch("https://apifalsa.vercel.app/api/routes/listar", {
     method: "GET",
-    mode: "no-cors",
+    // mode: "no-cors",
     headers: {
       'Content-Type': 'application/json',
       "titulo": "todos",
